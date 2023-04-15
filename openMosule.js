b840da5aa6c8be74ec823b7156ab07bc99d8e507
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-04-15 13:30:07 -0300

    ...

--- openMosule.js
+++ openMosule.js
@@ -1,7 +1,32 @@
 const closeBtn = document.getElementById("close");
+const overlay = document.getElementById("overlay");
+const saibaMais = document.getElementById("saiba-mais");
+const modulo = document.getElementById("modulo");
+
+let template = `<img src="./assets/icons/close.svg" id="close" class="close" alt="" /><h2>${title}</h2> <p>
+O câmbio de 6 marchas da CB 500X oferece maior confiança e performance
+na pilotagem, tornando as subidas de marcha mais leves e as reduções
+mais suaves!
+</p> <img src="./assets/img/moto2.svg" alt="" />
+<button class="btn quero-assinar">Quero assinar</button>`;
 
 closeBtn.addEventListener("click", () => {
-  let overlay = closeBtn.parentNode.parentNode;
+  overlay.classList.add("change");
+  setTimeout(() => {
+    overlay.style.display = "none";
+    overlay.classList.remove("change");
+  }, 500);
+});
 
-  console.log(typeof overlay);
+saibaMais.addEventListener("click", () => {
+  overlay.style.display = "block";
+
+  let filhos = cardBody.childNodes;
+  let title = filhos.item(0).textContent;
+
+  modulo.innerHTML = template;
+  overlay.classList.add("aparecer");
+  setTimeout(() => {
+    overlay.classList.remove("aparecer");
+  }, 1000);
 });
